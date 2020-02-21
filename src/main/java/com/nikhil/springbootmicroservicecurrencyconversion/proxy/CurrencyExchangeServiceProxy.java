31669c1ac731d1a0b80b6bc3bedbffe483fba745
Nikhil S. Zurunge <nikhilzurunge8@gmail.com>
2020-02-21 18:01:09 +0530

    added Eureka Naming Service for Load Balancing

--- src/main/java/com/nikhil/springbootmicroservicecurrencyconversion/proxy/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/nikhil/springbootmicroservicecurrencyconversion/proxy/CurrencyExchangeServiceProxy.java
@@ -4,10 +4,7 @@ import com.nikhil.springbootmicroservicecurrencyconversion.model.CurrencyConvers
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
